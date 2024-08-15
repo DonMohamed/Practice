ea399f43d46483285fdccf9d3e0fa6e1e677aa82
Mohamed Redda <donmohamed15@outlook.ccom>
2024-08-15 06:14:55 +0300

    Resolve Issus3

--- tests/test_add_task.py
+++ tests/test_add_task.py
@@ -13,7 +13,8 @@ def setup(request):
     options.add_argument('--disable-dev-shm-usage')  # Disable shared memory usage
     options.add_argument('--no-sandbox')  # Disable sandbox mode (not secure, but for testing purposes)
     #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
     request.cls.driver = driver
     yield
     driver.quit()
